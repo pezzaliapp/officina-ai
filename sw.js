0631baaee1a3264796bcf8fb3d909a7a0cf2e31a
pezzaliapp <pezzalialessandro@gmail.com>
2026-04-09 12:38:40 +0200

    fix: svuota service worker per forzare aggiornamento

--- sw.js
+++ sw.js
@@ -1,10 +1,1 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-    .then(() => self.clients.claim())
-  );
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request.url + '?v=' + Date.now()));
-});
+
